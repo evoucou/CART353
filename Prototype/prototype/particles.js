566023e8206c781905837ebda7328607e5176afc
evoucou <evoucou@users.noreply.github.com>
2018-03-04 13:25:57 -0500

    File names

--- Prototype/prototype/particles.js
+++ Prototype/prototype/particles.js
@@ -1,4 +1,4 @@
-var bottom = 600;
+var bottom = 300;
 
 function Particle(x, y, mass, displayColor) {
 
@@ -10,44 +10,62 @@ function Particle(x, y, mass, displayColor) {
   this.fallRate = map(this.mass, pMinMass, pMaxMass, 0.1, 0.05);
 
 
-  this.getBoundingBox = function() {
-    var radius = this.mass/2;
-    var offset = 2; // Just to expand its bb a bit.
+  //this.getBoundingBox = function() {
+  //  var radius = this.mass/2;
+  //  var offset = 2; // Just to expand its bb a bit.
 
-    var ax = this.pos.x-radius-offset;
-    var ay = this.pos.y-radius-offset;
-    var bx = this.pos.x+radius+offset;
-    var by = this.pos.y+radius+offset;
+  //  var ax = this.pos.x-radius-offset;
+  //  var ay = this.pos.y-radius-offset;
+  //  var bx = this.pos.x+radius+offset;
+  //  var by = this.pos.y+radius+offset;
 
-    return [ax, ay, bx, by];
-  }
+  //  return [ax, ay, bx, by];
+  //}
 
   this.move = function() {
-    var gravity = new p5.Vector(0, this.fallRate);
-    this.acc.add(gravity);
 
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     this.acc.mult(0);
+
+    var gravity = new p5.Vector(0, this.fallRate);
+    this.acc.add(gravity);
   }
 
-  //this.resolveCollisions = function() {
-  //  var hit_bottom = false;
 
-    //var bb1 = this.getBoundingBox();
+  //this.checkBottom = function() {
+  //  if (this.pos.y > 500) {
+  //    console.log("plus que 500");
+  //    this.vel.x *= -1;
+  //    var gravityBounce = new p5.Vector(0, 0.1);
+  //    this.acc.add(gravityBounce);
+  //  }
+  //}
 
-    //for (var c = 0; c < collisions.length; c++) {
-    // var col = collisions[c];
-    //}
+  //if (this.pos.y > windowHeight) {
+  //  twindowHeight.vel *= -1;
+  //  thiwindowHeightos.y = wHeight;
+  //}
 
-    //  var bb2 = collision.getBoundingBox();
 
-    //  // First check bounding box to optimize checks.
-    //  var is_close_enough = do_aabb_collision(bb1[0], bb1[1], bb1[2], bb1[3],
-    //                                          bb2[0], bb2[1], bb2[2], bb2[3]);
+  //this.resoeCollisions = function() {
+  //  var hit_bottom = fae;
 
-    //if (is_close_enough) {
-    //  var distance = dist(this.pos.x, this.pos.y, col.pos.x, col.pos.y);
+  //  var bb1 = this.getBoundingBox();
+
+
+  // for (var c = 0; c < collisis.length; c++) {
+  //   var col = collidedBottom[c];
+  //  }
+
+  //    var bb2 = collision.getBoundingBox();
+
+  //    // First check bounding box to optimize checks.
+  //    var is_close_enough = do_aabb_collision(bb1[0], bb1[1], bb1[2], bb1[3],
+  //                                            bb2[0], bb2[1], bb2[2], bb2[3]);
+
+  //  if (is_close_enough) {
+  //    var distance = dist(this.pos.x, this.pos.y, col.pos.x, col.pos.y);
 
   //  if (this.pos.y > bottom) {
   //    // Push out of collision object.
@@ -92,7 +110,6 @@ function Particle(x, y, mass, displayColor) {
   //    //break;
   //  }
   //  return hit_bottom;
-  //}
   //}
 
   this.display = function() {
---
+++ Prototype/prototype/prototype.js
@@ -0,0 +1,135 @@
+/*
+Waterfall cascades
+ 
+ Simulation of water colliding against rocks, and splitting to smaller drops.
+ 
+ Author:
+ Jason Labbe
+ 
+ Site:
+ jasonlabbe3d.com
+ */
+
+var pMinMass = 2;
+var pMaxMass = 8;
+var cMinMass = 15;
+var cMaxMass = 100;
+
+var waterfallMin;
+var waterfallMax;
+
+var particles = [];
+//var collisions;
+var bottomCollision;
+
+
+var fps;
+
+
+function do_aabb_collision(ax, ay, Ax, Ay, bx, by, Bx, By) {
+  return ! ((Ax < bx) || (Bx < ax) || (Ay < by) || (By < ay));
+}
+
+
+//// Set new targets for collision objects to lerp to.
+//function setNewLayout() {
+//  for (var i = 0; i < collisions.length; i++) {
+//    collisions[i].target.x = random(width/2.4, width-width/2.4);
+//    collisions[i].target.y = random(height/2, height);
+//    collisions[i].targetMass = random(cMinMass, cMaxMass);
+//  }
+//}
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  waterfallMin = width/2.6;
+  waterfallMax = width-width/2.6;
+
+  //  // Create collision objects.
+  //  for (var i = 0; i < 10; i++) {
+  //    var x = random(waterfallMin, waterfallMax);
+  //    var y = random(height/2, height);
+  //    var mass = random(cMinMass, cMaxMass);
+  //  }
+}
+
+
+function draw() {
+  background(0, 150);
+
+  var spawnCount = 20;
+
+  colorMode(HSB, 360);
+
+  // Spawn new particles.
+  for (var i = 0; i < spawnCount; i++) {
+    var x = random(waterfallMin, waterfallMax);
+    var mass = random(pMinMass, pMaxMass);
+
+    if (particles.length % 5 == 0) {
+      var displayColor = color(255);
+    } else {
+      var displayColor = color(random(180, 210), 255, 255);
+    }
+
+    var newParticle = new Particle(x, 0, mass, displayColor);
+    particles[particles.length] = newParticle;
+  }
+
+  colorMode(RGB, 255);
+
+  // Here, the loop looks like this because we are checking the particles in reverse.
+  // We have to do this, or else, we will skip a 'number' in our array, since we are deleting one.
+  for (var i = particles.length-1; i > -1; i--) {
+    //particles[i].move();     //var bottomCollision =    
+    //particles[i].resolveCollisions();
+    particles[i].display();
+    //particles[i].checkBottom();
+    
+      if (particles[i].pos.y > 500) {
+    bottomCollision = true;
+  } else {
+    bottomCollision = false;
+  }
+
+   if (bottomCollision) {
+      console.log("plus que 500");
+      particles[i].vel.x *= -1;
+      
+      var gravityBounce = new p5.Vector(0, 0.1);
+      particles[i].acc.add(gravityBounce);
+   } else {
+       particles[i].move();   
+   }
+
+    //collidedBottom() {
+    //particles[i].pos.y > particles[i].bottom;
+    //}
+
+    //if (particles[i].collidedBottom()) {
+    //System.out.println("collided with bottom");
+    //}
+
+    //  if (particles[i].pos.y > particles[i].bottom) {
+    //  // Splice allows us to delete position i at just one element.
+    //  particles[i].splice(i, 1);
+    //}
+
+    // else if (has_collision && particles[i].vel.mag() < 0.1) {
+    //  // Delete if it's stuck on top of a collision object.
+    //  particles.splice(i, 1);
+    //}
+  }
+
+
+  // Avoid updating frame rate every frame (not as readable).
+  if (frameCount % 10 == 0) {
+    fps = frameRate().toFixed(2);
+  }
+
+  // Display all ui items.
+  noStroke();
+  fill(255);
+}
