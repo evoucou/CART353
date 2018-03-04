f5effc9eca520b368a4406e10db6635a856576a7
evoucou <evoucou@users.noreply.github.com>
2018-03-04 14:50:29 -0500

    Trying to create foam by making the bottom particles fade slowly

---
+++ Prototype/prototype/prototype_js/prototype_js.js
@@ -0,0 +1,133 @@
+/*
+Waterfall cascades
+ 
+ Simulation of water colliding against rocks, and splitting to smaller drops.
+ 
+ Author:
+ Jason Labbe
+ 
+ Site:
+ jasonlabbe3d.com
+ */
+
+var pMinMass = 2;
+var pMaxMass = 8;
+var cMinMass = 15;
+var cMaxMass = 100;
+
+var waterfallMin;
+var waterfallMax;
+
+var particles = [];
+var bottomCollision;
+
+var fps;
+
+
+function do_aabb_collision(ax, ay, Ax, Ay, bx, by, Bx, By) {
+  return ! ((Ax < bx) || (Bx < ax) || (Ay < by) || (By < ay));
+}
+
+
+//// Set new targets for collision objects to lerp to.
+//function setNewLayout() {
+//  for (var i = 0; i < collisions.length; i++) {
+//    collisions[i].target.x = random(width/2.4, width-width/2.4);
+//    collisions[i].target.y = random(height/2, height);
+//    collisions[i].targetMass = random(cMinMass, cMaxMass);
+//  }
+//}
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  waterfallMin = width/2.6;
+  waterfallMax = width-width/2.6;
+
+  //  // Create collision objects.
+  //  for (var i = 0; i < 10; i++) {
+  //    var x = random(waterfallMin, waterfallMax);
+  //    var y = random(height/2, height);
+  //    var mass = random(cMinMass, cMaxMass);
+  //  }
+}
+
+
+function draw() {
+  background(0, 150);
+
+  var spawnCount = 20;
+
+  colorMode(HSB, 360);
+
+  // Spawn new particles.
+  for (var i = 0; i < spawnCount; i++) {
+    var x = random(waterfallMin, waterfallMax);
+    var mass = random(pMinMass, pMaxMass);
+
+    //if (particles.length % 5 == 0) {
+
+    //  var displayColor = color(255,);
+    //} else {
+    //  var displayColor = color(random(180, 210), 255, 255);
+    //}
+
+    var newParticle = new Particle(x, 0, mass);
+    particles[particles.length] = newParticle;
+  }
+
+  colorMode(RGB, 255);
+
+  // Here, the loop looks like this because we are checking the particles in reverse.
+  // We have to do this, or else, we will skip a 'number' in our array, since we are deleting one.
+  for (var i = particles.length-1; i > -1; i--) {
+    particles[i].display();
+
+    if (particles[i].pos.y > windowHeight - 100) {
+      bottomCollision = true;
+    } else {
+      bottomCollision = false;
+    }
+
+    if (bottomCollision) {
+      particles[i].alpha -= 5; 
+      console.log(particles[i].alpha);
+
+      //particles[i].vel.x *= -1;      
+      //var gravityBounce = new p5.Vector(0, 0.1);
+      //particles[i].acc.add(gravityBounce);
+    } else {
+      particles[i].alpha = 255;
+      particles[i].move();
+    }
+
+    if (particles[i].alpha < 0) {
+      particles.splice(i, 1);
+    }
+
+    //collidedBottom() {
+    //particles[i].pos.y > particles[i].bottom;
+    //}
+
+    //if (particles[i].collidedBottom()) {
+    //System.out.println("collided with bottom");
+    //}
+
+    //  if (particles[i].pos.y > particles[i].bottom) {
+    //  // Splice allows us to delete position i at just one element.
+    //  particles[i].splice(i, 1);
+    //}
+
+    // else if (has_collision && particles[i].vel.mag() < 0.1) {
+    //  // Delete if it's stuck on top of a collision object.
+    //  particles.splice(i, 1);
+    //}
+  }
+
+
+  // Avoid updating frame rate every frame (not as readable).
+  if (frameCount % 10 == 0) {
+    fps = frameRate().toFixed(2);
+  }
+}
