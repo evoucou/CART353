c8449771badc7ebafcdbb70bb604f8fac39fc7c7
evoucou <evoucou@users.noreply.github.com>
2018-04-15 19:26:45 -0400

    Changed some animations + added elephant crying
    Changed fluidity of some animations + elephant sad when water runs out

--- Final/animals.js
+++ Final/animals.js
@@ -8,14 +8,14 @@ function Animal(x, y, id) {
   this.y = y;
   this.id = id;
 
-
   this.load = function() {
 
     this.img = loadImage("data/" + this.id + "/" + this.id + "_walk1.png");
     this.walk = loadAnimation("data/" + this.id + "/" + this.id + "_walk1.png", "data/" + this.id + "/" + this.id + "_walk12.png");
-    this.bend = loadAnimation("data/" + this.id + "/" + this.id + "_drink1.png", "data/" + this.id + "/" + this.id + "_drink12.png");
-    this.reverseBend = loadAnimation("data/" + this.id + "/reverse/" + this.id + "_drink1.png", "data/" + this.id + "/reverse/" + this.id + "_drink12.png");
-    this.drink = loadAnimation("data/" + this.id + "/" + this.id + "_water1.png", "data/" + this.id + "/" + this.id + "_water8.png");
+    this.bend = loadAnimation("data/" + this.id + "/" + this.id + "_bend1.png", "data/" + this.id + "/" + this.id + "_bend8.png");
+    this.reverseBend = loadAnimation("data/" + this.id + "/reverse/" + this.id + "_bend1.png", "data/" + this.id + "/reverse/" + this.id + "_bend8.png");
+    this.drink = loadAnimation("data/" + this.id + "/" + this.id + "_drink1.png", "data/" + this.id + "/" + this.id + "_drink8.png");
+    this.cry = loadAnimation("data/" + this.id + "/" + this.id + "_cry1.png", "data/" + this.id + "/" + this.id + "_cry10.png");
     this.bend.looping = false;
     this.reverseBend.looping = false;
   }
@@ -28,32 +28,36 @@ function Animal(x, y, id) {
 
   this.drinking = function(animalX) {
     this.reverseBend.rewind();
+
     var frame;
     if (this.x == animalX) {
       this.vx = 0;
       this.walk.stop();
       this.walk.visible = false;
       animation(this.bend, this.x, this.y);
+      this.bend.visible = true;
       frame = this.bend.getFrame();
       console.log("frame: " + frame);
-      if (frame == 11) {
+      if (frame == 7) {
         animation(this.drink, this.x, this.y);
+        this.bend.visible = false;
         console.log("drink");
       }
     }
-
   }
 
   this.standing = function() {
     this.bend.rewind();
 
-    var frame;
+    //var frame;
     animation(this.reverseBend, this.x, this.y);
+    this.reverseBend.visible = true;
     frame = this.reverseBend.getFrame();
     console.log("reverse frame: " + frame);
-    //if (frame == 11) {
-    //image(this.img, this.x, this.y);
-    //console.log("stand");
-    //}
+    if (frame == 7) {
+        animation(this.cry, this.x, this.y);
+        this.reverseBend.visible = false;
+        console.log("cry");
+    }
   }
 }
